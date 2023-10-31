efa3f1d867f6e3368895b92e5efb5e706fb68aab
samimnif <samimnif@icloud.com>
2023-10-31 00:20:44 -0400

    Added cards and details about every flight

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for, Response
 import json
+import urllib.request, json
 import os
 import uuid
 from datetime import datetime
 from functools import wraps
 
 app = Flask(__name__)
+url = 'http://fr24.local:8754/flights.json'
+
+def get_data():
+    response = urllib.request.urlopen(url)
+    data = json.loads(response.read())
+    print(data)
+    return data
 
 @app.route('/')
 def index():
-    
-    return render_template('home.html')
+    return render_template('home.html', flights=get_data())
+
+@app.route('/flights', methods=['POST'])
+def flights():
+    return get_data()
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port="80", debug=True)
